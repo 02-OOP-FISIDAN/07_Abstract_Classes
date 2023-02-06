7981dd8029ec4e67996d9c381f1b0c0caf6cfc6d
FISIDAN <schwark@posteo.de>
2023-02-06 12:17:09 +0100

    alle Methoden und Klassen implementiert

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,22 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Triangle triangle = new Triangle(10, 10);
+        Circle circle = new Circle(10);
+
+        output("------ Flächen ------");
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Dreieck: " + triangle.area());
+        output("Fläche Kreis: " + circle.area()); 
+
+
+
+
+
+
+
     }
 
     private static void output(String outputStr) {

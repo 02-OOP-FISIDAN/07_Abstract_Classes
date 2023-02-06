7e65882fd95c3c300e457177cee59d949f83c2a3
FISIDAN <schwark@posteo.de>
2023-02-06 12:51:49 +0100

    final

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -17,15 +17,46 @@ public class App {
 
         output("Diff. r-t; " + areaDiff(rectangle, triangle));
         output("Diff. r-c: " + areaDiff(rectangle, circle));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
+        output("Diff. t-t: " + areaDiff(triangle, triangle));
+        
+        
+        output("------ Summen ------");
+
+        output("Sum. r+t; " + areaSum(rectangle, triangle));
+        output("Sum. r+c: " + areaSum(rectangle, circle));
+        output("Sum. c+t: " + areaSum(circle, triangle));
+        output("Sum. t+t: " + areaSum(triangle, triangle));
+    
     }
 
-    private static double areaDiff(Rectangle r, Triangle t) {
-        return r.area() - t.area();
-    }
+
+    private static double areaDiff(Shape s1, Shape s2) {
+            return s1.area() - s2.area();
+        }
+
+        private static double areaSum(Shape s1, Shape s2) {
+            return s1.area() + s2.area();
+    
+}
+
+
+    // Überladen. okay bei einige wenigen
+    // private static double areaDiff(Rectangle r, Triangle t) {
+    //     return r.area() - t.area();
+    // }
+
+
+    // 
     // private static double areaDiff(Rectangle r, Circle c) {
     //     return r.area() - c.area();
     // }
 
+    
+    
+    
+    
+    
     private static void output(String outputStr) {
         System.out.println(outputStr);
 

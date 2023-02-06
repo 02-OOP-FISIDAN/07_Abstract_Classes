c70933a050a2861f173ffada65731ff40217c151
FISIDAN <schwark@posteo.de>
2023-02-06 12:30:30 +0100

    überladene Methode areaDiff

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,7 +1,7 @@
 package com.cc.java;
 
 public class App {
-    
+
     public static void main(String[] args) {
 
         Rectangle rectangle = new Rectangle(10.0, 10.0);
@@ -11,21 +11,24 @@ public class App {
         output("------ Flächen ------");
         output("Fläche Rechteck: " + rectangle.area());
         output("Fläche Dreieck: " + triangle.area());
-        output("Fläche Kreis: " + circle.area()); 
+        output("Fläche Kreis: " + circle.area());
 
+        output("------ Differenzen ------");
 
-
-
-
-
-
+        output("Diff. r-t; " + areaDiff(rectangle, triangle));
+        output("Diff. r-c: " + areaDiff(rectangle, circle));
     }
+
+    private static double areaDiff(Rectangle r, Triangle t) {
+        return r.area() - t.area();
+    }
+    // private static double areaDiff(Rectangle r, Circle c) {
+    //     return r.area() - c.area();
+    // }
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
+
     }
 
-
-
 }
-
